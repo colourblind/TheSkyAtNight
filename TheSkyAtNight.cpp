3ba3a3f0ce8f91e8684da8f7c782493e444188e8
colourblind <tom.milsom@gmail.com>
2011-01-19 19:16:54 +0000

    Global tinting. Refactored out star drawing. Provisional nebula gumf

--- TheSkyAtNight.cpp
+++ TheSkyAtNight.cpp
@@ -3,7 +3,6 @@
 #include "cinder/ImageIo.h"
 #include "cinder/Rand.h"
 #include "cinder/Utilities.h"
-#include "cinder/gl/Fbo.h"
 #include "cinder/gl/Texture.h"
 
 #include <string>
@@ -23,14 +22,17 @@ public:
 
 private:
     void RenderStars();
+    void RenderNebulae();
     void RenderClouds();
+
+    void DrawStar(float size);
 
     gl::Texture smallStarTexture_;
     gl::Texture mediumStarTexture_;
     gl::Texture largeStarTexture_;
     gl::Texture hugeStarTexture_;
     gl::Texture cloudTexture_;
-    gl::Fbo fbo_;
+    Color tint_;
     bool done_;
 };
 
@@ -42,7 +44,6 @@ void TheSkyAtNight::setup()
     else
         Rand::randomize();
 
-    gl::enableAdditiveBlending();
     gl::disableDepthRead();
     gl::disableDepthWrite();
 
@@ -63,14 +64,12 @@ void TheSkyAtNight::setup()
 
 void TheSkyAtNight::draw()
 {
-//    CameraPersp camera = CameraPersp(512, 512, 90, 1, 10);
-
     if (!done_)
     {
         int r = Rand::randInt(0, 2);
         int g = Rand::randInt(0, 2);
         int b = Rand::randInt(0, 2);
-        float tintStrength = Rand::randFloat(0, 1);
+        tint_ = ColorA(r, g, b);
 
         // gl::clear(Color(r * tintStrength * 0.1f, g * tintStrength * 0.1f, b * tintStrength * 0.1f));
         gl::clear();
@@ -79,10 +78,8 @@ void TheSkyAtNight::draw()
         camera.lookAt(Vec3f(0, 0, 0), Vec3f(0, 0, -1));
         gl::setMatrices(camera);
 
-        gl::color(Color(r * tintStrength, g * tintStrength, b * tintStrength));
-        // gl::color(ColorA(255, 255, 255, 32));
         RenderClouds();
-
+        RenderNebulae();
         RenderStars();
 
         done_ = true;
@@ -114,65 +111,83 @@ void TheSkyAtNight::draw()
 
 void TheSkyAtNight::RenderStars()
 {
+    gl::enableAdditiveBlending();
     smallStarTexture_.enableAndBind();
     int numSmallStars = Rand::randInt(10000, 100000);
     for (int i = 0; i < numSmallStars; i ++)
-    {
-        float x = Rand::randFloat(-3, 3);
-        float y = Rand::randFloat(-3, 3);
-        float z = Rand::randFloat(-3, 3);
-        float r = Rand::randFloat(0.85f, 1);
-        float g = Rand::randFloat(0.85f, 1);
-        float b = Rand::randFloat(0.85f, 1);
-        gl::color(Color(r, g, b));
-        gl::drawBillboard(Vec3f(x, y, z), Vec2f(0.01f, 0.01f), 0, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
-    }
+        DrawStar(0.01f);
 
     mediumStarTexture_.enableAndBind();
     int numMediumStars = Rand::randInt(200, 5000);
     for (int i = 0; i < numMediumStars; i ++)
-    {
-        float x = Rand::randFloat(-3, 3);
-        float y = Rand::randFloat(-3, 3);
-        float z = Rand::randFloat(-3, 3);
-        float r = Rand::randFloat(0.85f, 1);
-        float g = Rand::randFloat(0.85f, 1);
-        float b = Rand::randFloat(0.85f, 1);
-        gl::color(Color(r, g, b));
-        gl::drawBillboard(Vec3f(x, y, z), Vec2f(0.04f, 0.04f), 0, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
-    }
+        DrawStar(0.04f);
 
     largeStarTexture_.enableAndBind();
     int numLargeStars = Rand::randInt(20, 200);
     for (int i = 0; i < numLargeStars; i ++)
-    {
-        float x = Rand::randFloat(-3, 3);
-        float y = Rand::randFloat(-3, 3);
-        float z = Rand::randFloat(-3, 3);
-        float r = Rand::randFloat(0.85f, 1);
-        float g = Rand::randFloat(0.85f, 1);
-        float b = Rand::randFloat(0.85f, 1);
-        gl::color(Color(r, g, b));
-        gl::drawBillboard(Vec3f(x, y, z), Vec2f(0.08f, 0.08f), 0, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
-    }
+        DrawStar(0.08f);
 
     hugeStarTexture_.enableAndBind();
     int numHugeStars = Rand::randInt(0, 4);
     for (int i = 0; i < numHugeStars; i ++)
+        DrawStar(0.3f);
+}
+
+void TheSkyAtNight::DrawStar(float size)
+{
+    float x = Rand::randFloat(-3, 3);
+    float y = Rand::randFloat(-3, 3);
+    float z = Rand::randFloat(-3, 3);
+    float r = Rand::randFloat(0.85f, 1);
+    float g = Rand::randFloat(0.85f, 1);
+    float b = Rand::randFloat(0.85f, 1);
+    gl::color(Color(r, g, b));
+    gl::drawBillboard(Vec3f(x, y, z), Vec2f(size, size), 0, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
+}
+
+void TheSkyAtNight::RenderNebulae()
+{
+    gl::enableAlphaBlending();
+    gl::enableAdditiveBlending(); // temp
+    int numNebula = Rand::randInt(0, 10);
+    numNebula = 1;
+    for (int i = 0; i < numNebula; i ++)
     {
         float x = Rand::randFloat(-3, 3);
         float y = Rand::randFloat(-3, 3);
         float z = Rand::randFloat(-3, 3);
-        float r = Rand::randFloat(0.85f, 1);
-        float g = Rand::randFloat(0.85f, 1);
-        float b = Rand::randFloat(0.85f, 1);
-        gl::color(Color(r, g, b));
-        gl::drawBillboard(Vec3f(x, y, z), Vec2f(0.3f, 0.3f), 0, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
+//        Vec3f pos = Vec3f(x, y, z);
+        Vec3f pos = Vec3f(0, 0, -2);
+
+        int numClouds = Rand::randInt(10, 50);
+        for (int i = 0; i < numClouds; i ++)
+        {
+            float x1 = Rand::randFloat(0, 1);
+            float y1 = Rand::randFloat(0, 1);
+            float z1 = Rand::randFloat(0, 1);
+            x1 = x1 * x1 - 0.5f; // Square the offsets to make them tend towards the centre
+            y1 = y1 * y1 - 0.5f;
+            z1 = z1 * z1 - 0.5f;
+            Vec3f offset = Vec3f(x1, y1, z1);
+            float scaleX = Rand::randFloat(0.1f, 0.5f);
+            float scaleY = Rand::randFloat(0.1f, 0.5f);
+            Vec2f scale = Vec2f(scaleX, scaleY);
+            float rotate = Rand::randFloat(0, 360);
+
+            float r = Rand::randFloat(0, 1);
+            float b = Rand::randFloat(0, 1);
+            float g = Rand::randFloat(0, 1);
+            float a = Rand::randFloat(0, 1);
+
+            gl::color(ColorA(tint_.r + r, tint_.g + g, tint_.b + b, a));
+            gl::drawBillboard(pos + offset, scale, rotate, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
+        }
     }
 }
 
 void TheSkyAtNight::RenderClouds()
 {
+    gl::enableAdditiveBlending();
     cloudTexture_.enableAndBind();
     int numClouds = Rand::randInt(3, 15);
     for (int i = 0; i < numClouds; i ++)
@@ -182,6 +197,7 @@ void TheSkyAtNight::RenderClouds()
         float z = Rand::randFloat(-5, 5);
         float size = Rand::randFloat(4, 10);
         float rotation = Rand::randFloat(0, 360);
+        gl::color(tint_ * 0.25f);
         gl::drawBillboard(Vec3f(x, y, z), Vec2f(size, size), rotation, Vec3f(1, 0, 0), Vec3f(0, 1, 0));
     }
 }
