b7bc69c3d76aa8ab3e57d0a715ec37c2a5e170cf
colourblind <tom.milsom@gmail.com>
2011-02-10 23:05:22 +0000

    Fixed paths after src move

--- src/Resources.h
+++ src/Resources.h
@@ -3,11 +3,11 @@
 
 // This seems to ignore the additional include folder setting. Yay for 
 // relative paths.
-#include "../cinder/include/cinder/CinderResources.h"
+#include "../../cinder/include/cinder/CinderResources.h"
 
-#define RES_STAR_TEXTURE0	CINDER_RESOURCE(resources/, star0.png, 128, GLSL)
-#define RES_STAR_TEXTURE1	CINDER_RESOURCE(resources/, star1.png, 129, GLSL)
-#define RES_STAR_TEXTURE2   CINDER_RESOURCE(resources/, star2.png, 130, GLSL)
-#define RES_CLOUD_TEXTURE0	CINDER_RESOURCE(resources/, cloud0.png, 131, GLSL)
+#define RES_STAR_TEXTURE0	CINDER_RESOURCE(../resources/, star0.png, 128, GLSL)
+#define RES_STAR_TEXTURE1	CINDER_RESOURCE(../resources/, star1.png, 129, GLSL)
+#define RES_STAR_TEXTURE2   CINDER_RESOURCE(../resources/, star2.png, 130, GLSL)
+#define RES_CLOUD_TEXTURE0	CINDER_RESOURCE(../resources/, cloud0.png, 131, GLSL)
 
 #endif // THESKYATNIGHT_RESOURCES_H
